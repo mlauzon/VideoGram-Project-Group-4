979f0c768f8224da19034aac82e27a3d8c4dbb60
Melissa Lauzon <lauzonmelissap@gmail.com>
2018-09-22 13:21:37 -0700

    Initial Commit

---
+++ VideoGram Project Group 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoGram Project Group 4
+//
+//  Created by student on 9/22/18.
+//  Copyright © 2018 The-Windows-Specialists. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
